7226135aabad2d39df5da4965b5a8af59fd99823
anggi267 <anggipermanaputra267@gmail.com>
2024-03-23 01:49:32 +0700

    paket lsp

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>Anggi Tour</title>
+        <meta name="description" content="">
+<!-- 
+Travel Template
+http://www.templatemo.com/tm-409-travel
+-->
+        <meta name="viewport" content="width=device-width">
+		<meta name="author" content="templatemo">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,800,700,600,300' rel='stylesheet' type='text/css'>
+        
+        <link rel="stylesheet" href="/anggi/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/anggi/css/font-awesome.css">
+        <link rel="stylesheet" href="/anggi/css/animate.css">
+        <link rel="stylesheet" href="/anggi/css/templatemo_misc.css">
+        <link rel="stylesheet" href="/anggi/css/templatemo_style.css">
+
+        <script src="/anggi/js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an outdated browser. <a href="http://browsehappy.com/">Upgrade your browser today</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to better experience this site.</p>
+        <![endif]-->
+
+        <div class="site-header">
+            <div class="container">
+                <div class="main-header">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6 col-xs-10">
+                            <div class="logo">
+                                <a href="#">
+                                    <img src="/anggi/images/logo.png" alt="travel html5 template" title="travel html5 template">
+                                </a>
+                            </div> <!-- /.logo -->
+                        </div> <!-- /.col-md-4 -->
+                        <div class="col-md-8 col-sm-6 col-xs-2">
+                            <div class="main-menu">
+                                <ul class="visible-lg visible-md">
+                                    <!-- <li class="active"><a href="index.html">Home</a></li> -->
+                                    <li class="active"><a href={{ route('register') }}>Register</a></li>
+                                    <li class="active"><a href={{ route('login') }}>Login</a></li>
+                                    <!-- <li><a href="services.html">Services</a></li>
+                                    <li><a href="events.html">Events</a></li>
+                                    <li><a href="about.html">About Us</a></li>
+                                    <li><a href="contact.html">Contact</a></li> -->
+                                </ul>
+                                <a href="#" class="toggle-menu visible-sm visible-xs">
+                                    <i class="fa fa-bars"></i>
+                                </a>
+                            </div> <!-- /.main-menu -->
+                        </div> <!-- /.col-md-8 -->
+                    </div> <!-- /.row -->
+                </div> <!-- /.main-header -->
+                <div class="row">
+                    <div class="col-md-12 visible-sm visible-xs">
+                        <div class="menu-responsive">
+                            <ul>
+                                <!-- <li class="active"><a href="index.html">Home</a></li> -->
+                                <li class="active"><a href={{ route('register') }}>Register</a></li>
+                                <li class="active"><a href={{ route('login') }}>Login</a></li>
+                                <!-- <li><a href="services.html">Services</a></li>
+                                <li><a href="events.html">Events</a></li>
+                                <li><a href="about.html">About Us</a></li>
+                                <li><a href="contact.html">Contact</a></li> -->
+                            </ul>
+                        </div> <!-- /.menu-responsive -->
+                    </div> <!-- /.col-md-12 -->
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </div> <!-- /.site-header -->
+
+
+        <div class="flexslider">
+            <ul class="slides">
+                <li>
+                    <div class="overlay"></div>
+                    <img src="/anggi/images/templatemo_slide_1.jpg" alt="Special 1">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-5 col-lg-4">
+                                <div class="flex-caption visible-lg">
+                                    <!-- <span class="price">$7,500</span> -->
+                                    <h3 class="title">Venice, Italy</h3>
+                                    <p>Venice, atau Venezia dalam bahasa Italia, adalah sebuah kota yang memukau yang terletak di bagian timur 
+                                        laut Italia. Dibangun di atas sekelompok 118 pulau kecil di Laguna Venesia, kota ini terkenal dengan 
+                                        jaringan kanalnya yang rumit, arsitektur yang menakjubkan, dan warisan budayanya yang kaya. Venice sering 
+                                        dianggap sebagai salah satu kota paling romantis dan unik di dunia, yang menarik jutaan pengunjung setiap 
+                                        tahunnya. 
+                                    </p> 
+                            			<!-- <a rel="nofollow" href="http://unsplash.com" target="_parent">Unsplash</a>.</p> -->
+                                    <!-- <a href="#" class="slider-btn">Pre-booking</a> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="overlay"></div>
+                    <img src="/anggi/images/templatemo_slide_2.jpg" alt="Special 2">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-5 col-lg-4">
+                                <div class="flex-caption visible-lg">
+                                    <!-- <span class="price">$12,400</span> -->
+                                    <h3 class="title">Barcelona, Spain</h3>
+                                    <p>Barcelona merupakan kota yang menakjubkan yang terletak di pesisir timur laut Spanyol. Ibukota Catalonia 
+                                        ini terkenal karena keindahan arsitektur yang unik, budaya yang kaya, pantai-pantai yang menawan, dan 
+                                        kehidupan malam yang hidup. Barcelona adalah salah satu tujuan wisata paling populer di Eropa, menarik 
+                                        jutaan pengunjung setiap tahunnya.</p>
+                                    <!-- <a href="#" class="slider-btn">Reserve Now</a> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="overlay"></div>
+                    <img src="/anggi/images/templatemo_slide_3.jpg" alt="Special 3">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-5 col-lg-4">
+                                <div class="flex-caption visible-lg">
+                                    <!-- <span class="price">$20,800</span> -->
+                                    <h3 class="title">Basel, Switzerland</h3>
+                                    <p>Basel adalah sebuah kota yang indah yang terletak di sebelah barat daya Swiss, berbatasan dengan Perancis 
+                                        dan Jerman. Kota ini terletak di tepi Sungai Rhein dan merupakan salah satu pusat keuangan, budaya, dan 
+                                        pendidikan terpenting di Swiss. Basel adalah kota terbesar ketiga di negara ini dan menawarkan kekayaan 
+                                        seni, arsitektur yang memukau, festival-festival terkenal, dan keragaman budaya.</p>
+                                    <!-- <a href="#" class="slider-btn">Book Now</a> -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div> <!-- /.flexslider -->
+
+        
+        
+        <div class="container">
+            <div class="row">
+                <div class="our-listing owl-carousel">
+                    <div class="list-item">
+                        <div class="list-thumb">
+                            <div class="title">
+                                <h4>Italy</h4>
+                            </div>
+                            <img src="/anggi/images/destination_1.jpg" alt="destination 1">
+                        </div> <!-- /.list-thumb -->
+                        <div class="list-content">
+                            <h5>Rome, Milan, Naples</h5>
+                            <!-- <span>SILVER HOTEL, BINTANG 5</span> -->
+                            <!-- <a href="#" class="price-btn">$1,700 Book Now</a> -->
+                        </div> <!-- /.list-content -->
+                    </div> <!-- /.list-item -->
+                    <div class="list-item">
+                        <div class="list-thumb">
+                            <div class="title">
+                                <h4>France</h4>
+                            </div>
+                            <img src="/anggi/images/destination_2.jpg" alt="destination 2">
+                        </div> <!-- /.list-thumb -->
+                        <div class="list-content">
+                            <h5>Paris, Marseille, Lyon</h5>
+                            <!-- <span>NEW PALACE, BINTANG 5</span> -->
+                            <!-- <a href="#" class="price-btn">$2,200 Book Now</a> -->
+                        </div> <!-- /.list-content -->
+                    </div> <!-- /.list-item -->
+                    <div class="list-item">
+                        <div class="list-thumb">
+                            <div class="title">
+                                <h4>Germany</h4>
+                            </div>
+                            <img src="/anggi/images/destination_3.jpg" alt="destination 3">
+                        </div> <!-- /.list-thumb -->
+                        <div class="list-content">
+                            <h5>Berlin, Hamburg, Munich</h5>
+                            <!-- <span>LUXE HOTEL, BINTANG 5</span> -->
+                            <!-- <a href="#" class="price-btn">$3,300 Book Now</a> -->
+                        </div> <!-- /.list-content -->
+                    </div> <!-- /.list-item -->
+                    <div class="list-item">
+                        <div class="list-thumb">
+                            <div class="title">
+                                <h4>Spain</h4>
+                            </div>
+                            <img src="/anggi/images/destination_4.jpg" alt="destination 4">
+                        </div> <!-- /.list-thumb -->
+                        <div class="list-content">
+                            <h5>Madrid, Bercelona, Valencia</h5>
+                            <!-- <span>GOOD HOTEL, BINTANG 5</span> -->
+                            <!-- <a href="#" class="price-btn">$4,400 Book Now</a> -->
+                        </div> <!-- /.list-content -->
+                    </div> <!-- /.list-item -->
+                    <div class="list-item">
+                        <div class="list-thumb">
+                            <div class="title">
+                                <h4>Netherlands</h4>
+                            </div>
+                            <img src="/anggi/images/destination_5.jpg" alt="destination 5">
+                        </div> <!-- /.list-thumb -->
+                        <div class="list-content">
+                            <h5>Amsterdam, Delft, The Hague</h5>
+                            <!-- <span>BEST HOTEL, BINTANG 5</span> -->
+                            <!-- <a href="#" class="price-btn">$5,500 Book Now</a> -->
+                        </div> <!-- /.list-content -->
+                    </div> <!-- /.list-item -->
+                    <div class="list-item">
+                        <div class="list-thumb">
+                            <div class="title">
+                                <h4>Switzerland</h4>
+                            </div>
+                            <img src="/anggi/images/destination_6.jpg" alt="destination 6">
+                        </div> <!-- /.list-thumb -->
+                        <div class="list-content">
+                            <h5>Zürich, Geneva, Basel</h5>
+                            <!-- <span>NEW HOTEL, 7 NIGHTS, 7 STARS</span> -->
+                            <!-- <a href="#" class="price-btn">$6,600 Book Now</a> -->
+                        </div> <!-- /.list-content -->
+                    </div> <!-- /.list-item -->
+                </div> <!-- /.our-listing -->
+            </div> <!-- /.row -->
+        </div> <!-- /.container -->
+
+		<div class="middle-content"></div>
+
+        <div class="partner-list">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2 col-sm-4 col-xs-6">
+                        <div class="partner-item">
+                            <img src="/anggi/images/partners/partner1.png" alt="">
+                        </div> <!-- /.partner-item -->
+                    </div> <!-- /.col-md-2 -->
+                    <div class="col-md-2 col-sm-4 col-xs-6">
+                        <div class="partner-item">
+                            <img src="/anggi/images/partners/partner2.png" alt="">
+                        </div> <!-- /.partner-item -->
+                    </div> <!-- /.col-md-2 -->
+                    <div class="col-md-2 col-sm-4 col-xs-6">
+                        <div class="partner-item">
+                            <img src="/anggi/images/partners/partner3.png" alt="">
+                        </div> <!-- /.partner-item -->
+                    </div> <!-- /.col-md-2 -->
+                    <div class="col-md-2 col-sm-4 col-xs-6">
+                        <div class="partner-item">
+                            <img src="/anggi/images/partners/partner1.png" alt="">
+                        </div> <!-- /.partner-item -->
+                    </div> <!-- /.col-md-2 -->
+                    <div class="col-md-2 col-sm-4 col-xs-6">
+                        <div class="partner-item">
+                            <img src="/anggi/images/partners/partner2.png" alt="">
+                        </div> <!-- /.partner-item -->
+                    </div> <!-- /.col-md-2 -->
+                    <div class="col-md-2 col-sm-4 col-xs-6">
+                        <div class="partner-item last">
+                            <img src="/anggi/images/partners/partner3.png" alt="">
+                        </div> <!-- /.partner-item -->
+                    </div> <!-- /.col-md-2 -->
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </div> <!-- /.partner-list -->
+
+		
+
+        <div class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-sm-4">
+                        <div class="footer-logo">
+                            <a href="index.html">
+                                <img src="/anggi/images/logo.png" alt="">
+                            </a>
+                        </div>
+                    </div> <!-- /.col-md-4 -->
+                    <div class="col-md-4 col-sm-4">
+                        <div class="copyright">
+                            <span>
+                            	
+                                Copyright &copy; 2023 <a href="#">Anggi TOUR</a>
+                            
+                            
+                            <!--
+                            | Design: <a rel="nofollow" href="http://www.templatemo.com" target="_parent">templatemo</a>
+                            -->
+                            
+                            </span>
+                        </div>
+                    </div> <!-- /.col-md-4 -->
+                    <!-- <div class="col-md-4 col-sm-4">
+                        <ul class="social-icons">
+                            <li><a href="#" class="fa fa-facebook"></a></li>
+                            <li><a href="#" class="fa fa-twitter"></a></li>
+                            <li><a href="#" class="fa fa-linkedin"></a></li>
+                            <li><a href="#" class="fa fa-flickr"></a></li>
+                            <li><a href="#" class="fa fa-rss"></a></li>
+                        </ul> -->
+                    <!-- </div> /.col-md-4 -->
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </div> <!-- /.site-footer -->
+
+        <script src="/anggi/js/vendor/jquery-1.11.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="/anggi/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="/anggi/js/bootstrap.js"></script>
+        <script src="/anggi/js/plugins.js"></script>
+        <script src="/anggi/js/main.js"></script>
+        <!-- templatemo 409 travel -->  
+    </body>
+</html>
